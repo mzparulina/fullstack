82adf787654d248a8afca86cb27981b33ec2f3e4
mzparulina <pauline.revilla@gmail.com>
2023-07-02 22:38:26 -0400

    Added edit article component

---
+++ frontend/src/app/editarticle/editarticle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editarticle',
+  templateUrl: './editarticle.component.html',
+  styleUrls: ['./editarticle.component.scss']
+})
+export class EditarticleComponent {
+
+}
