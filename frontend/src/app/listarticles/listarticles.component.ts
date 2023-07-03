b62d2271b2f446f4d863e885656624eee148a41d
mzparulina <pauline.revilla@gmail.com>
2023-07-02 22:38:44 -0400

    Added article list component

---
+++ frontend/src/app/listarticles/listarticles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarticles',
+  templateUrl: './listarticles.component.html',
+  styleUrls: ['./listarticles.component.scss']
+})
+export class ListarticlesComponent {
+
+}
