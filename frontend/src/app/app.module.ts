5fd9e53d685856a23624621213359cc4ff7c5ada
mzparulina <pauline.revilla@gmail.com>
2023-07-08 08:29:01 -0400

    Remove services/article component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarticlesComponent } from './listarticles/listarticles.component';
+import { CreatearticleComponent } from './createarticle/createarticle.component';
+import { EditarticleComponent } from './editarticle/editarticle.component';
+import { ArticleComponent } from './services/article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarticlesComponent,
+    CreatearticleComponent,
+    EditarticleComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
