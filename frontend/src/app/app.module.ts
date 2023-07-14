8c3022d3d0067e55ab052ec74e4871782cfbb362
mzparulina <pauline.revilla@gmail.com>
2023-07-14 07:49:27 -0400

    Added reactivesmodule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarticlesComponent } from './listarticles/listarticles.component';
@@ -18,7 +18,9 @@ import { EditarticleComponent } from './editarticle/editarticle.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
