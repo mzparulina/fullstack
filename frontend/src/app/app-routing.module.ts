13d1ba2ed3dc29c8e87f35ed269feb510000669c
mzparulina <pauline.revilla@gmail.com>
2023-07-15 16:44:25 -0400

    created routes for create/articles

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarticlesComponent  } from './listarticles/listarticles.component';
+import { CreatearticleComponent } from './createarticle/createarticle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ListarticlesComponent
+  },
+  {
+    path: 'create/article', component: CreatearticleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
